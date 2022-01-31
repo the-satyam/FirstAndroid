a86e3f0a79dfad1d3684b5883e4a17290490dafe
Satyam2 <satyamvishwa03@gmail.com>
2022-01-31 11:17:58 +0530

    passing data between activities

--- app/src/main/java/com/example/firstandroid/HomeActivity.kt
+++ app/src/main/java/com/example/firstandroid/HomeActivity.kt
@@ -3,18 +3,23 @@ package com.example.firstandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var name = intent.extras?.getString("sk")
+        var tvhome = findViewById<TextView>(R.id.tvhome)
+        tvhome.text = name
     }
 
     fun sendHandle(view: android.view.View) {
         Toast.makeText(this, "Sending data", Toast.LENGTH_SHORT).show()
         var intent: Intent    //kotlin says you can't have a variable as null
         intent = Intent(this,MainActivity::class.java)  //no need to give the word new
+        intent.putExtra("sv","satyam vishwakarma")
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/firstandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String name = getIntent().getExtras().getString("ds");
+        TextView mTextView = findViewById(R.id.textView2);
+        mTextView.setText(name);
     }
 
     public void loginHandler(View view) {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActivity.class);
+        mIntent.putExtra("s2","satyam in java");
         startActivity(mIntent);
     }
 }
