461a63000884213f452505227037d49239d599ef
Satyam2 <satyamvishwa03@gmail.com>
2022-01-31 12:07:52 +0530

    intent to invoke activity

--- app/src/main/java/com/example/firstandroid/HomeActivity.kt
+++ app/src/main/java/com/example/firstandroid/HomeActivity.kt
@@ -1,25 +1,40 @@
 package com.example.firstandroid
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         var name = intent.extras?.getString("sk")
-        var tvhome = findViewById<TextView>(R.id.tvhome)
+        var tvhome = findViewById<TextView>(R.id.tvHome)
         tvhome.text = name
     }
 
     fun sendHandle(view: android.view.View) {
-        Toast.makeText(this, "Sending data", Toast.LENGTH_SHORT).show()
-        var intent: Intent    //kotlin says you can't have a variable as null
-        intent = Intent(this,MainActivity::class.java)  //no need to give the word new
-        intent.putExtra("sv","satyam vishwakarma")
-        startActivity(intent)
+        when(view.id){
+            R.id.btnSend -> {  startDialer() }
+            R.id.btnMain -> {startMain() }
+        }
     }
+
+private fun startDialer() {
+    var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:21345678"))
+    startActivity(dialIntent)
+}
+
+
+private fun startMain() {
+    Toast.makeText(this, "Sending data", Toast.LENGTH_SHORT).show()
+    var intent: Intent    //kotlin says you can't have a variable as null
+    intent = Intent(this,MainActivity::class.java)  //no need to give the word new
+    intent.putExtra("sv","satyam vishwakarma")
+    startActivity(intent)
+}
 }
--- app/src/main/java/com/example/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/firstandroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,15 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name = getIntent().getExtras().getString("ds");
-        TextView mTextView = findViewById(R.id.textView2);
+        TextView mTextView = findViewById(R.id.tvMain);
         mTextView.setText(name);
     }
 
     public void loginHandler(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                startHome();
+                break;
+            case R.id.btnCancel:
+                createAlarm("bajaj",14,29);
+                break;
+        }
+    }
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void startHome() {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActivity.class);
         mIntent.putExtra("s2","satyam in java");
-        startActivity(mIntent);
     }
 }
